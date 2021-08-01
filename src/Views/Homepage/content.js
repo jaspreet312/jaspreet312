7c18566ae0c00b6dd0694e6dcc24e633f87184f5
jaspreet312 <88236138+jaspreet312@users.noreply.github.com>
2021-07-31 21:52:27 -0700

    Added Categories Section

--- src/Views/Homepage/content.js
+++ src/Views/Homepage/content.js
@@ -1,62 +1,135 @@
 const CONTENT = {
-  mainHeading: "Cashback Loot Lo",
-  cardsData: [
-    {
-      productCard: (
-        <iframe
-          marginwidth="0"
-          marginheight="0"
-          scrolling="no"
-          frameborder="0"
-          src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
-        ></iframe>
-      ),
-    },
-    {
-      productCard: (
-        <iframe
-          marginwidth="0"
-          marginheight="0"
-          scrolling="no"
-          frameborder="0"
-          src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
-        ></iframe>
-      ),
-    },
-    {
-      productCard: (
-        <iframe
-          marginwidth="0"
-          marginheight="0"
-          scrolling="no"
-          frameborder="0"
-          src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
-        ></iframe>
-      ),
-    },
-    {
-      productCard: (
-        <iframe
-          marginwidth="0"
-          marginheight="0"
-          scrolling="no"
-          frameborder="0"
-          src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
-        ></iframe>
-      ),
-    },
-    {
-      productCard: (
-        <iframe
-          marginwidth="0"
-          marginheight="0"
-          scrolling="no"
-          frameborder="0"
-          src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
-        ></iframe>
-      ),
-    },
-  ],
+  mainHeading: "Cashback Offers",
+  categories: {
+    "Hot Offers": [
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+    ],
+
+    Mobiles: [
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+    ],
+
+    Laptops: [
+      {
+        productName: "RGB Keyboard",
+        productCard: (
+          <iframe
+            marginwidth="0"
+            marginheight="0"
+            scrolling="no"
+            frameborder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=qf_sp_asin_til&ad_type=product_link&tracking_id=jaspreet0b4-21&marketplace=amazon&amp;region=IN&placement=B082KR95Z4&asins=B082KR95Z4&linkId=1d3e3bb0aaee451e4e840afa7dc5b90e&show_border=true&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff"
+          ></iframe>
+        ),
+      },
+    ],
+  },
 };
 
 export default CONTENT;
--- src/Views/Homepage/index.js
+++ src/Views/Homepage/index.js
@@ -3,22 +3,32 @@ import CONTENT from "./content";
 import styles from "./style.module.css";
 
 function HomePage() {
-  const { mainHeading, cardsData } = CONTENT;
+  const { mainHeading, categories } = CONTENT;
+
+  console.log(categories);
   return (
     <>
       <div className={styles.parentWrapper}>
         <div className={styles.headingWrapper}>{mainHeading}</div>
-        <div className={styles.cardsWrapper}>
-          {cardsData.map((card, index) => {
-            return (
-              <div className={styles.cardWrapper} key={index}>
-                <div className={styles.productCardWrapper}>
-                  {card.productCard}
-                </div>
+
+        {Object.keys(categories).map((category) => {
+          return (
+            <div className={styles.categoryWrapper}>
+              <div className={styles.categoryHeading}>{category}</div>
+              <div className={styles.cardsWrapper}>
+                {categories[category].map((card, index) => {
+                  return (
+                    <div className={styles.cardWrapper} key={index}>
+                      <div className={styles.productCardWrapper}>
+                        {card.productCard}
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
